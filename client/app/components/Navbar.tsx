e03917427b43b94541a46864fc037060487176f0
itsfloki <coder.abhijit90@gmail.com>
2023-09-14 09:26:32 +0530

    update: navbar; add: login page

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useRef, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function Navbar() {
   const darkIconNode = useRef<SVGSVGElement>(null)
@@ -52,16 +53,16 @@ export default function Navbar() {
   return (
     <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             className="h-8 mr-3"
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Talky
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-solid-bg"
           onClick={toggleNav}
@@ -95,30 +96,30 @@ export default function Navbar() {
         >
           <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <li className="p-2.5 sm:py-1">
-              <a
-                href="#"
+              <Link
+                href="/login"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Login
-              </a>
+              </Link>
             </li>
 
             <li className="p-2.5 sm:py-1">
-              <a
+              <Link
                 href="#"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Register
-              </a>
+              </Link>
             </li>
 
             <li className="p-2.5 sm:py-1">
-              <a
+              <Link
                 href="#"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
-              </a>
+              </Link>
             </li>
 
             <li>
